854b280600f3cac4fd648697617e09d029bd76ef
Jean-Baptiste Guerin <75306270+Sharkoux@users.noreply.github.com>
2021-06-15 20:41:51 +0200

    Update cryptovaleur.ps1

--- cryptovaleur.ps1
+++ cryptovaleur.ps1
@@ -2,15 +2,14 @@
 
 $contenu3 = read-host "Voulez vous continuer? [OUI]? "
 while ($contenu3 -eq "OUI") {
-$crypt = read-host "Quel est la crypto dont vous recherchez la valeur? "
+$crypt = read-host "Quel est la crypto dont vous recherchez la valeur? (Exemple: BTC/ETH/USDT ect)"
 
-$fiat = Read-Host "En quel monnaie fiat souhaitez vous voir cette valeur? "
+$fiat = Read-Host "En quel monnaie fiat souhaitez vous voir cette valeur? (Exemple: EUR/USDT)"
 
-Invoke-RestMethod -Uri "https://api.binance.com/api/v3/ticker/price?symbol=$crypt$fiat" -Method get  -OutFile "D:\Bureau\test2.txt"
 
-$contenu = Get-Content -Path D:\Bureau\test2.txt
+$contenu = Invoke-RestMethod -Uri "https://api.binance.com/api/v3/ticker/price?symbol=$crypt$fiat" -Method get  
 
-$contenu2 = "$contenu".Split(",")[-1].Split(":""}")[-2] 
+$contenu2 = "$contenu".Split("}").Split("=").Split()[-2]
 
 Read-Host "La valeur de $crypt est de $contenu2 $fiat ! "
 
